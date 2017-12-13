5d9a7a46e45cf834d1e7d156576c36ca18ff7e33
whyguu <whyguu@sina.com>
2017-12-13 12:47:57 +0800

    Add files via upload

---
+++ cut_tiff.py
@@ -0,0 +1,31 @@
+﻿# coding：utf-8
+# Bin GAO
+
+import os
+import gdal
+
+# input path
+in_path = './test/'
+input_filename = 'luhe.tiff'
+
+# output path
+out_path = './output/'
+output_filename = 'luhe_'
+
+# patch size
+tile_size_width = 1024
+tile_size_height = 1024
+
+
+ds = gdal.Open(in_path+input_filename)
+band = ds.GetRasterBand(1)
+x_size = band.XSize
+y_size = band.YSize
+
+for i in range(0, x_size, tile_size_width):
+    for j in range(0, y_size, tile_size_height):
+        cut = "gdal_translate -of GTIFF -srcwin " + str(i)+ ", " + str(j) + ", " \
+              + str(tile_size_width) + ", " + str(tile_size_height) + " " \
+              + str(in_path) + str(input_filename) + " " \
+              + str(out_path) + str(output_filename) + str(i) + "_" + str(j) + ".tif"
+        os.system(cut)
